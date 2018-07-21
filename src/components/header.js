ad4d5eec229d7a72ad53d3f0b8f5a3e52b3a86bc
Meng To <mengto@Mengs-MacBook-Pro.local>
2018-07-21 18:07:45 -0400

    End of Adaptive Layouts

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,6 +14,7 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
+    <Header />
     {children()}
   </div>
 )
