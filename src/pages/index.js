f095062888caa9f09a454507ee4b51e0760cf024
Meng To <mengto@Mengs-MacBook-Pro.local>
2018-07-21 14:35:44 -0400

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
