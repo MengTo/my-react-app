158e12f14f05cf96711100498e380408e396aa65
Tiago Mergulhão <me@tmergulhao.com>
2018-08-12 14:54:59 -0400

    Requiring images is unpolite

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,12 +36,12 @@ const IndexPage = () => (
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
         <Link to="/page-2/">Watch the video</Link>
         <div className="Logos">
-          <img src={require('../images/logo-sketch.png')} width="50" />
-          <img src={require('../images/logo-figma.png')} width="50" />
-          <img src={require('../images/logo-studio.png')} width="50" />
-          <img src={require('../images/logo-framer.png')} width="50" />
-          <img src={require('../images/logo-react.png')} width="50" />
-          <img src={require('../images/logo-swift.png')} width="50" />
+          <img src='/images/logo-sketch.png' width="50" />
+          <img src='/images/logo-figma.png' width="50" />
+          <img src='/images/logo-studio.png' width="50" />
+          <img src='/images/logo-framer.png' width="50" />
+          <img src='/images/logo-react.png' width="50" />
+          <img src='/images/logo-swift.png' width="50" />
         </div>
         <Wave />
       </div>
@@ -52,24 +52,24 @@ const IndexPage = () => (
         <Card 
           title="Design System"
           text="10 sections"
-          image={require('../images/wallpaper.jpg')} />
+          image='/images/wallpaper.jpg' />
         <Card 
           title="React for Designers"
           text="12 sections"
-          image={require('../images/wallpaper2.jpg')} />
+          image='/images/wallpaper2.jpg' />
         <Card 
           title="Sound Design"
           text="5 sections"
-          image={require('../images/wallpaper3.jpg')} />
+          image='/images/wallpaper3.jpg' />
         <Card 
           title="ARKit 2"
           text="10 sections"
-          image={require('../images/wallpaper4.jpg')} />
+          image='/images/wallpaper4.jpg' />
       </div>
     </div>
     <Section
-      image={require('../images/wallpaper2.jpg')}
-      logo={require('../images/logo-react.png')}
+      image='/images/wallpaper2.jpg'
+      logo='/images/logo-react.png'
       title="React for Designers"
       text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
      />
